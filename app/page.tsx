5650a46d0f6d38a0bc14915dce67768405249a57
Kerem Uryan <aunstonegar@gmail.com>
2025-12-30 17:10:36 +0300

    post processing

--- app/page.tsx
+++ app/page.tsx
@@ -3,57 +3,44 @@
 import { useMemo, useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import * as THREE from "three";
-import { EffectComposer, Bloom, Vignette, Noise, ToneMapping } from "@react-three/postprocessing";
-import { BlendFunction } from "postprocessing";
+import { EffectComposer, Bloom, Vignette, Noise } from "@react-three/postprocessing";
 
 function easeInOutCubic(t: number) {
   return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
 }
 
-function Scene({
-  index,
-  doorXs,
-}: {
-  index: number;
-  doorXs: number[];
-}) {
+function Scene({ index, doorXs }: { index: number; doorXs: number[] }) {
   const { camera } = useThree();
 
   const camY = 1.6;
   const camZ = 5.2;
 
-  // "Süzülme" animasyonu state'i (ref ile frame bağımsız)
   const prevIndexRef = useRef(index);
   const fromXRef = useRef(doorXs[index]);
   const toXRef = useRef(doorXs[index]);
-  const tRef = useRef(1); // 1 = animasyon yok
-  const durationRef = useRef(0.85); // saniye
+  const tRef = useRef(1);
+  const durationRef = useRef(0.85);
 
   const lookAt = useMemo(() => new THREE.Vector3(), []);
 
-  // index değişince yeni geçiş başlat
   if (prevIndexRef.current !== index) {
     prevIndexRef.current = index;
-
-    // mevcut kameranın anlık X'ini "from" kabul et (geçiş ortasında bile tıklarsa kopmaz)
     fromXRef.current = camera.position.x;
     toXRef.current = doorXs[index];
     tRef.current = 0;
   }
 
   useFrame((_, delta) => {
-    // t'yi 0->1 akıt
     if (tRef.current < 1) {
       tRef.current = Math.min(1, tRef.current + delta / durationRef.current);
       const eased = easeInOutCubic(tRef.current);
       const x = fromXRef.current + (toXRef.current - fromXRef.current) * eased;
       camera.position.set(x, camY, camZ);
     } else {
-      // animasyon yoksa hedefte sabit tut
       camera.position.set(toXRef.current, camY, camZ);
     }
 
-    // her zaman seçili kapıya bak
+    // düz koridora bak: orbit hissi yok
     lookAt.set(camera.position.x, 1.2, 0);
     camera.lookAt(lookAt);
   });
@@ -63,44 +50,41 @@ function Scene({
       <ambientLight intensity={1.2} />
       <directionalLight position={[5, 8, 5]} intensity={1.8} />
 
+      {/* Zemin */}
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[4, 0, 0]}>
         <planeGeometry args={[60, 60]} />
-        <meshStandardMaterial
-  color={i === index ? "#ffffff" : "#f1f1f1"}
-  emissive={i === index ? "#ffffff" : "#000000"}
-  emissiveIntensity={i === index ? 2.5 : 0}
-/>
+        <meshStandardMaterial color="#ffffff" />
       </mesh>
 
+      {/* Kapılar */}
+      {doorXs.map((x, i) => {
+        const selected = i === index;
+        return (
+          <group key={i} position={[x, 0, 0]}>
+            {/* Çerçeve */}
+            <mesh position={[0, 1.1, 0]}>
+              <boxGeometry args={[1.35, 2.35, 0.12]} />
+              <meshStandardMaterial color="#efefef" />
+            </mesh>
 
+            {/* İç panel (seçili olan glow atar) */}
+            <mesh position={[0, 1.1, 0.07]}>
+              <boxGeometry args={[1.15, 2.1, 0.05]} />
+              <meshStandardMaterial
+                color={selected ? "#ffffff" : "#f1f1f1"}
+                emissive={selected ? "#ffffff" : "#000000"}
+                emissiveIntensity={selected ? 6 : 0}
+              />
+            </mesh>
 
-      {doorXs.map((x, i) => (
-        <group key={i} position={[x, 0, 0]}>
-          <mesh position={[0, 1.1, 0]}>
-            <boxGeometry args={[1.35, 2.35, 0.12]} />
-            <meshStandardMaterial
-  color={i === index ? "#ffffff" : "#f1f1f1"}
-  emissive={i === index ? "#ffffff" : "#000000"}
-  emissiveIntensity={i === index ? 2.5 : 0}
-/>
-
-          </mesh>
-
-          <mesh position={[0, 1.1, 0.07]}>
-            <boxGeometry args={[1.15, 2.1, 0.05]} />
-            <meshStandardMaterial
-  color={i === index ? "#ffffff" : "#f1f1f1"}
-  emissive={i === index ? "#ffffff" : "#000000"}
-  emissiveIntensity={i === index ? 2.5 : 0}
-/>
-          </mesh>
-
-          <mesh position={[0, 2.45, 0.07]}>
-            <boxGeometry args={[0.8, 0.18, 0.05]} />
-            <meshStandardMaterial color="#ffffff" />
-          </mesh>
-        </group>
-      ))}
+            {/* Etiket */}
+            <mesh position={[0, 2.45, 0.07]}>
+              <boxGeometry args={[0.8, 0.18, 0.05]} />
+              <meshStandardMaterial color="#ffffff" />
+            </mesh>
+          </group>
+        );
+      })}
     </>
   );
 }
@@ -114,20 +98,18 @@ export default function Home() {
 
   return (
     <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
-      <Canvas
-  camera={{ position: [doorXs[0], 1.6, 5.2], fov: 50 }}
-  gl={{ antialias: true }}
->
-  <color attach="background" args={["#f8f8f8"]} />
-  <Scene index={index} doorXs={doorXs} />
+      <Canvas camera={{ position: [doorXs[0], 1.6, 5.2], fov: 50 }} gl={{ antialias: true }}>
+        <color attach="background" args={["#f8f8f8"]} />
 
-  <EffectComposer multisampling={0}>
-    <Bloom intensity={1.2} luminanceThreshold={0.0} luminanceSmoothing={0.0} />
-    <Vignette eskil={false} offset={0.15} darkness={1.4} />
-    <Noise opacity={0.25} />
-  </EffectComposer>
-</Canvas>
+        <Scene index={index} doorXs={doorXs} />
 
+        {/* Postprocess (gözle görülür) */}
+        <EffectComposer multisampling={0}>
+          <Bloom intensity={1.5} luminanceThreshold={0.0} luminanceSmoothing={0.0} />
+          <Vignette eskil={false} offset={0.15} darkness={1.4} />
+          <Noise opacity={0.25} />
+        </EffectComposer>
+      </Canvas>
 
       <div style={{ position: "absolute", inset: 0, pointerEvents: "none" }}>
         <button
